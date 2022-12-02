300d2c6fbcee3ecf9332cee8b969e3b323a4fad6
Kim Thompson <kim.thompson.dev@protonmail.com>
2022-12-02 02:52:13 +0000

    tried to mess around with stuff like pattern matching make the code terser (though not more legible, I think!)

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,41 +1,30 @@
 defmodule AdventOfCode.Day01 do
 
-  # Split a string into integers and add them up
-  def sum_calories(snacks) do
-    Enum.sum(Enum.map(String.split(snacks, "\n", trim: true), fn (snack) -> String.to_integer(snack) end))
-  end
-
   def get_elves do
     input = AdventOfCode.Input.get!(1)
     Enum.with_index(String.split(input, "\n\n", trim: true))
   end
 
-  def part1(_args) do
-    elves = get_elves()
+  def sum_calories(snacks) do
+    Enum.sum(Enum.map(String.split(snacks, "\n", trim: true), fn s -> String.to_integer(s) end))
+  end
 
-    # Assign each elf a name and sum the calories in their snacks
-    elf_packs = Enum.map(elves, fn ({snacks, name}) -> %{
+  def rank_elves(elves) do
+    Enum.sort_by(Enum.map(elves, fn ({snacks, name}) -> %{
       :calories => sum_calories(snacks),
       :name => name + 1
-    } end)
+    } end), &(&1[:calories]), &>=/2)
+  end
 
-    # Figure out which pack has the most calories
-    Enum.max_by(elf_packs, fn pack -> pack[:calories] end)[:calories]
+  def part1(_args) do
+    elves = get_elves()
+    [%{calories: calories} | _others] = rank_elves(elves)
+    calories
   end
 
   def part2(_args) do
     elves = get_elves()
-
-    # Assign each elf a name and sum the calories in their snacks
-    elf_packs = Enum.map(elves, fn({snacks, name}) -> %{
-      :calories => sum_calories(snacks),
-      :name => name + 1
-    } end)
-
-    # Figure out which three elves have the most calories (sort descending, then grab first three)
-    most_prepared_elves = Enum.take(Enum.sort_by(elf_packs, &(&1[:calories]), &>=/2), 3)
-
-    # Return the sum of those calories
-    Enum.sum(Enum.map(most_prepared_elves, fn elf -> elf.calories end))
+    Enum.sum(Enum.map(Enum.take(rank_elves(elves), 3), &(&1.calories)))
   end
+
 end
