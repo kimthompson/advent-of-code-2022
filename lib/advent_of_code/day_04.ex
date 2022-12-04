39d53430f2a5123144b607bb660aafa01c05795a
Kim Thompson <kim.thompson.dev@protonmail.com>
2022-12-04 18:32:51 +0000

    day 4 solutions

--- lib/advent_of_code/day_04.ex
+++ lib/advent_of_code/day_04.ex
@@ -1,7 +1,40 @@
 defmodule AdventOfCode.Day04 do
+  @data AdventOfCode.Input.get!(4, 2022)
+    |> String.split("\n", trim: true)
+
+  def convert_to_ranges(assignments) do
+    [first_assignment, second_assignment] = assignments
+
+    [first_beginning, first_ending] = String.split(first_assignment, "-")
+    [second_beginning, second_ending] = String.split(second_assignment, "-")
+
+    first_range = MapSet.new(Enum.to_list(String.to_integer(first_beginning)..String.to_integer(first_ending)))
+    second_range = MapSet.new(Enum.to_list(String.to_integer(second_beginning)..String.to_integer(second_ending)))
+
+    [first_range, second_range]
+  end
+
   def part1(_args) do
+    Enum.reduce(@data, [], fn elf_pair, acc ->
+      assignments = elf_pair
+        |> String.split(",")
+        |> convert_to_ranges()
+
+      acc ++ [MapSet.subset?(List.first(assignments), List.last(assignments)) || MapSet.subset?(List.last(assignments), List.first(assignments))]
+    end)
+    |> Enum.count(&(&1))
   end
 
   def part2(_args) do
+    Enum.reduce(@data, [], fn elf_pair, acc ->
+      assignments = elf_pair
+        |> String.split(",")
+        |> convert_to_ranges()
+
+      has_overlap = MapSet.size(MapSet.intersection(MapSet.new(List.first(assignments)), MapSet.new(List.last(assignments)))) > 0
+
+      acc ++ [has_overlap]
+    end)
+    |> Enum.count(&(&1))
   end
 end
--- test/advent_of_code/day_04_test.exs
+++ test/advent_of_code/day_04_test.exs
@@ -3,19 +3,17 @@ defmodule AdventOfCode.Day04Test do
 
   import AdventOfCode.Day04
 
-  @tag :skip
   test "part1" do
-    input = nil
+    input = AdventOfCode.Input.get!(4, 2022)
     result = part1(input)
 
-    assert result
+    assert result == 588
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
+    input = AdventOfCode.Input.get!(4, 2022)
     result = part2(input)
 
-    assert result
+    assert result == 911
   end
 end
