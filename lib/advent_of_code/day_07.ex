c934385ba116d1b7c0fc1cc58a362ba8b2666063
Kim Thompson <kim.thompson.dev@protonmail.com>
2022-12-07 19:31:09 +0000

    scaffolded day 07

--- lib/advent_of_code/day_07.ex
+++ lib/advent_of_code/day_07.ex
@@ -1,5 +1,23 @@
 defmodule AdventOfCode.Day07 do
+  @data AdventOfCode.Input.get!(7, 2022)
+  @data "$ cd /\n$ ls\ndir a\n14848514 b.txt\n8504156 c.dat\ndir d\n$ cd a\n$ ls\ndir e\n29116 f\n2557 g\n62596 h.lst\n$ cd e\n$ ls\n584 i\n$ cd ..\n$ cd ..\n$ cd d\n$ ls\n4060174 j\n8033020 d.log\n5626152 d.ext\n7214296 k"
+    |> String.split("\n")
+
   def part1(_args) do
+    Enum.reduce(@data, [], fn x, acc ->
+      cond do
+        x =~ "cd" ->
+          acc ++ ["cd"]
+        x =~ "ls" ->
+          acc ++ ["ls"]
+        x =~ "dir" ->
+          acc ++ ["dir"]
+        Regex.match? ~r{\A\d}, x ->
+          acc ++ ["size"]
+        true ->
+          acc
+      end
+    end)
   end
 
   def part2(_args) do
